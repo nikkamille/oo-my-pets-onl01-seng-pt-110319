82ac7386f79e91446932e41d8b0fae87d39bfddd
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-12 11:55:52 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@cats = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@cats << self
+  end
+  
+  def self.all
+    @@cats
+  end
+  
+  def self.count
+    @@cats.count
+  end
+  
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@dogs = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@dogs << self
+  end
+  
+  def self.all
+    @@dogs
+  end
+  
+  def self.count
+    @@dogs.count
+  end
+  
 end
