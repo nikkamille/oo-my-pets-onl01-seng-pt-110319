71230635a978ecf22355bda12cc7eec4ebf5a09e
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-12 11:05:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,24 @@
 class Owner
   
   attr_reader :name, :species
+  @@owners = []
   
   def initialize
     @species = human
+    @@owners << self
   end
   
   def say_species
     puts "I am a #{species}."
   end  
+  
+  def self.all 
+    @@owners
+  end
+  
+  def self.count
+    @@owners.count
+  end
   
   
 end
